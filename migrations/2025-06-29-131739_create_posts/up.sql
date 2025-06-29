68949f50663bd8ebc5695b77f398bc439d47532b
ZiedYousfi <yousfi_zied@yahoo.com>
2025-06-29 15:47:15 +0200

    Update posts table migration: change id column to use AUTOINCREMENT

--- migrations/2025-06-29-131739_create_posts/up.sql
+++ migrations/2025-06-29-131739_create_posts/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 CREATE TABLE "posts"(
-	"id" INTEGER NOT NULL PRIMARY KEY,
+	"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	"title" TEXT NOT NULL,
 	"body" TEXT NOT NULL,
 	"published" BOOL NOT NULL
