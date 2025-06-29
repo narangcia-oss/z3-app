832fa34102e164c1f4764ae42e8b16fa66d39092
ZiedYousfi <yousfi_zied@yahoo.com>
2025-06-29 16:20:31 +0200

    Update posts table migration: change id column type to Int4 and add up/down SQL scripts

--- src/db/schema.rs
+++ src/db/schema.rs
@@ -1,11 +1,8 @@
 // @generated automatically by Diesel CLI.
 
-// diesel migration generate --diff-schema <migration_name>
-// diesel migration run
-
 diesel::table! {
     posts (id) {
-        id -> Integer,
+        id -> Int4,
         title -> Text,
         body -> Text,
         published -> Bool,
