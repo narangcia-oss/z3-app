223be3e06c143fb4b2b0fd9350a8f0f6811180db
ZiedYousfi <yousfi_zied@yahoo.com>
2025-06-30 10:10:52 +0200

    Add user authentication model and backend implementation; update dependencies in Cargo.toml

--- src/db/models.rs
+++ src/db/models.rs
@@ -8,7 +8,6 @@
 /// diesel migration run
 /// ```
 /// Then i recommend properly making your models here
-
 use diesel::prelude::*;
 use serde::Deserialize;
 
@@ -78,4 +77,123 @@ impl NewPost {
             published,
         }
     }
+}
+
+mod users {
+    use super::*;
+    use crate::db::schema::users as users_table;
+    use async_trait::async_trait;
+    use axum_login::{AuthUser, AuthnBackend, UserId};
+    use password_auth::verify_password;
+    use serde::{Deserialize, Serialize};
+    use tokio::task;
+
+    #[derive(Queryable, Selectable, Clone, Serialize, Deserialize)]
+    #[diesel(table_name = users_table)]
+    #[diesel(check_for_backend(diesel::pg::Pg))]
+    pub struct User {
+        pub id: i32,
+        pub username: String,
+        pub password: String,
+        pub email: Option<String>,
+    }
+
+    impl std::fmt::Debug for User {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.debug_struct("User")
+                .field("id", &self.id)
+                .field("username", &self.username)
+                .field("password", &"[redacted]")
+                .finish()
+        }
+    }
+
+    impl AuthUser for User {
+        type Id = i32;
+        fn id(&self) -> Self::Id {
+            self.id
+        }
+        fn session_auth_hash(&self) -> &[u8] {
+            self.password.as_bytes()
+        }
+    }
+
+    #[derive(Debug, Clone, Deserialize, Serialize)]
+    pub struct Credentials {
+        pub username: String,
+        pub password: String,
+        pub next: Option<String>,
+    }
+
+    #[derive(Clone)]
+    pub struct Backend {
+        pub db: std::sync::Arc<tokio::sync::Mutex<diesel::PgConnection>>,
+    }
+
+    impl Backend {
+        pub fn new(db: diesel::PgConnection) -> Self {
+            Self {
+                db: std::sync::Arc::new(tokio::sync::Mutex::new(db)),
+            }
+        }
+    }
+
+    #[derive(Debug, thiserror::Error)]
+    pub enum Error {
+        #[error(transparent)]
+        Diesel(#[from] diesel::result::Error),
+        #[error(transparent)]
+        TaskJoin(#[from] tokio::task::JoinError),
+    }
+
+    #[async_trait]
+    impl AuthnBackend for Backend {
+        type User = User;
+        type Credentials = Credentials;
+        type Error = Error;
+
+        async fn authenticate(
+            &self,
+            creds: Self::Credentials,
+        ) -> Result<Option<Self::User>, Self::Error> {
+            let db = self.db.clone();
+            let username = creds.username.clone();
+            let user: Option<User> = task::spawn_blocking(move || {
+                let mut conn = db.blocking_lock();
+                users_table::table
+                    .filter(users_table::username.eq(&username))
+                    .select(User::as_select())
+                    .first::<User>(&mut *conn)
+                    .ok()
+            })
+            .await?;
+
+            let password = creds.password;
+            let valid = user
+                .as_ref()
+                .map(|u| verify_password(password.clone(), &u.password).is_ok())
+                .unwrap_or(false);
+            Ok(if valid { user } else { None })
+        }
+
+        async fn get_user(
+            &self,
+            user_id: &UserId<Self>,
+        ) -> Result<Option<Self::User>, Self::Error> {
+            let db = self.db.clone();
+            let id = *user_id;
+            let user = task::spawn_blocking(move || {
+                let mut conn = db.blocking_lock();
+                users_table::table
+                    .filter(users_table::id.eq(id))
+                    .select(User::as_select())
+                    .first::<User>(&mut *conn)
+                    .ok()
+            })
+            .await?;
+            Ok(user)
+        }
+    }
+
+    pub type AuthSession = axum_login::AuthSession<Backend>;
 }
