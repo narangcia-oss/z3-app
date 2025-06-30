8069106271563d260090c01d30a5af7462162614
ZiedYousfi <yousfi_zied@yahoo.com>
2025-06-30 12:59:57 +0200

    Remove initial migration files and update schema to include accounts and sessions tables

--- src/db/schema.rs
+++ src/db/schema.rs
@@ -15,15 +15,54 @@ diesel::table! {
     users (id) {
         id -> Int4,
         username -> Text,
-        password -> Text,
-        email -> Nullable<Text>,
+
         created_at -> Timestamp,
+        accounts -> Integer
     }
 }
 
-diesel::joinable!(posts -> users (author_id));
+diesel::table! {
+    accounts (id) {
+        id -> Integer,
+        user_id -> Integer,
+        type_ -> Text, // Utiliser ce champ comme enum côté Rust
+        provider -> Nullable<Text>,
+        provider_account_id -> Nullable<Text>,
+        refresh_token -> Nullable<Text>,
+        access_token -> Nullable<Text>,
+        expires_at -> Nullable<Int4>,
+        token_type -> Nullable<Text>,
+        scope -> Nullable<Text>,
+        id_token -> Nullable<Text>,
+        session_state -> Nullable<Text>,
+        refresh_token_expires_in -> Nullable<Int4>,
+    }
+}
+
+diesel::table! {
+    sessions (id) {
+        id -> Text,
+        session_token -> Text,
+        user_id -> Text,
+        expires -> Timestamp,
+    }
+}
+
+diesel::table! {
+    verification_tokens (identifier, token) {
+        identifier -> Text,
+        token -> Text,
+        expires -> Timestamp,
+    }
+}
+
+// Relations
+diesel::joinable!(accounts -> users (user_id));
 
 diesel::allow_tables_to_appear_in_same_query!(
     posts,
     users,
+    accounts,
+    sessions,
+    verification_tokens,
 );
